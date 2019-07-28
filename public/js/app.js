9b53c5b57c8cc2be8ea3bba0426efc80a286a127
gregorywaynepower <gregorywpower@gmail.com>
2019-07-27 22:29:32 -0400

    Attempting to add JS features

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,10 @@ var clickedBtn = function() {
 menuBtn[0].addEventListener('click', clickedBtn)
 
 console.log(menuBtn[0])
+
+window.addEventListener('mouseup', function(event){
+    var box = document.getElementById('mobile-menu.active')
+    if(event.target != box && event.target.parentNode != box){
+        box.cssStyleSheet.display = 'none'
+    }
+})
